7846f7c9179b1dfc06c585905c71227b0e810be2
r05323045 <r05323045@ntu.edu.tw>
2020-12-20 23:15:27 +0800

    fetch APIs in /admin/users

--- src/apis/admin.js
+++ src/apis/admin.js
@@ -11,5 +11,10 @@ export default {
     return apiHelper.delete(`/admin/tweets/${tweetId}`, {
       headers: { Authorization: `Bearer ${getToken()}` }
     })
+  },
+  getUsers () {
+    return apiHelper.get('/admin/users', {
+      headers: { Authorization: `Bearer ${getToken()}` }
+    })
   }
 }
