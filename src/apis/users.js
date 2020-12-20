f8627607bb3b735e0bab18ae889c3ed416e60b2b
hcc3860 <hcc3860@gmail.com>
2020-12-21 00:31:37 +0800

    fetch APIs in UserSignUp

--- src/apis/users.js
+++ src/apis/users.js
@@ -1,6 +1,6 @@
 import { apiHelper } from './../utils/helpers'
 const getToken = () => localStorage.getItem('token')
-
+const userId = () => Response.data.user.id
 export default {
   getCurrentUser () {
     return apiHelper.get('/get_current_user', {
@@ -10,6 +10,16 @@ export default {
   get ({ userId }) {
     return apiHelper.get(`/users/${userId}`, {
       headers: { Authorization: `Bearer ${getToken()}` }
+    })
+  },
+  getSettingPage ({ userId }) {
+     return apiHelper.get(`/users/${userId}/setting`, {
+        headers: { Authorization: `Bearer ${getToken()}` }
+      })
+  },
+  putSetting ({ formData }) {
+    return apiHelper.put(`/users/${userId}/setting`, formData, {
+        headers: { Authorization: `Bearer ${getToken()}` }
     })
   }
 }
